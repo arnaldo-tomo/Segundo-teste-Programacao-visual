f6c8de6812692c3f3d73846edf295b7efccb5bbf
Arnaldo tomo <arnaldotomo@gmail.com>
2022-10-29 12:44:23 +0200

    Finalizado

--- Inventory/Properties/AssemblyInfo.cs
+++ Inventory/Properties/AssemblyInfo.cs
@@ -5,12 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Inventory")]
+[assembly: AssemblyTitle("GestaoDeEstoque")]
+// [assembly: AssemblyTitle("Inventory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Inventory")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("GestaoDeEstoque")]
+// [assembly: AssemblyProduct("Inventory")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
